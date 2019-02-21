6d8509dc80dfa9f8de787ab7b0ac89078153cf5c
ashish-rane <ashishrane@outlook.com>
2019-02-21 19:18:42 -0400

    2019-02-21

--- Regression/LinearRegression/LinearRegression_Scipy.py
+++ Regression/LinearRegression/LinearRegression_Scipy.py
@@ -13,26 +13,8 @@ from scipy.optimize import minimize
 J_hist = pd.DataFrame(columns =['theta', 'cost'])
 iterations = 0
 
-def costFunc(theta, X, y):
-    y_pred = X.dot(theta)
-    # Calculate mean squared error
-    mse = ((y_pred - y) ** 2).mean()
-    J_hist.loc[len(J_hist)] = [theta, mse]
-    
-    
-    
-    global iterations 
-    iterations = iterations + 1
-    return mse
-
-
-
 house_prices = pd.read_csv('house_prices.csv', header=None, 
                            names=['area', 'noOfBedrooms', 'price'])
-
-# initial theta
-initial_theta = np.ones(1).reshape(1,1)
-alpha = 0.01
 
 # features
 X = house_prices['area'].values
@@ -42,30 +24,66 @@ X = X.reshape(X.shape[0], 1)
 y = house_prices['price'].values
 y = y.reshape(y.shape[0], 1)
 
-# Assume Theta0 = 0
+
+# Visualize our data
+fig, axes = pp.subplots()
+axes.grid(color='b', alpha=0.8, linestyle='dotted', linewidth = 0.5)
+axes.scatter(X, y, marker='o', color='r')
+axes.set_xlabel('area')
+axes.set_ylabel('price')
+
+
+# Hypothesis function - h(x) = X.theta
+def hypothesisFunc(theta, X):
+    return X.dot(theta)
+
+
+# Cost Function - Mean Square Error
+def costFunc(theta, X, y):
+    m = X.shape[0]
+    y_pred = hypothesisFunc(theta,X)
+    y_pred = y_pred.reshape(y_pred.shape[0],1)
+    
+    # Calculate mean squared error
+    cost = np.sum((y_pred - y) ** 2)/ (2 * m)
+    J_hist.loc[len(J_hist)] = [theta, cost]
+    
+    
+    global iterations 
+    iterations = iterations + 1
+    return cost
+
+
+
+# initial theta (Assume Theta0 = 0)
+initial_theta = np.ones(1).reshape((1,1))
+
 # train our hypothesis to minimize cost
 res = minimize(costFunc, initial_theta, args=(X, y))
 
-theta = res.x
+theta = res.x.reshape((res.x.shape[0], 1))
 
 # Create some test data for which to predict
 xs = np.linspace(house_prices['area'].min(), house_prices['area'].max(), 10)
+xs = xs.reshape((xs.shape[0], 1))
 
 # ys = prediction for test values of xs
-ys = xs * theta
+ys = hypothesisFunc(theta, xs)
 
+# Visualize our prediction line
 fig, axes = pp.subplots(nrows=1, ncols=2, figsize=(15,4))
 ax1,ax2 = axes.flatten()
 ax1.grid(color='b', alpha=0.8, linestyle='dotted', linewidth = 0.5)
 ax2.grid(color='b', alpha=0.8, linestyle='dotted', linewidth = 0.5)
 
 ax1.scatter(X, y, marker='o', color='r')
-ax1.plot(xs,ys, color='green')
+ax1.plot(xs,ys, color='blue')
+ax1.scatter(xs,ys,color='green', s=100, marker='o')
 ax1.set_xlabel('area')
 ax1.set_ylabel('price')
 
+# Visualize the progress of the algorithm
 opacity = 0.2
-# plot the progess of the algorithm
 for t in J_hist['theta']:
     y = xs * t
     ax1.plot(xs, y, color='blue', alpha=opacity, linestyle='dashed')
--- Regression/LinearRegression/LinearRegression_Scipy_Intercept.py
+++ Regression/LinearRegression/LinearRegression_Scipy_Intercept.py
@@ -16,30 +16,45 @@ from scipy import interpolate
 J_hist = pd.DataFrame(columns =['theta', 'cost'])
 iterations = 0
 
-def costFunc(theta, X, y):
-    #h(x) = theta0* X0 + theta1*X1
-    #print('X:{0}, theta: {1}, y:{2}'.format(X.shape, theta.shape, y.shape))
-    y_pred = X.dot(theta)
-    # Calculate mean squared error
-    mse = ((y_pred - y) ** 2).mean()
-    J_hist.loc[len(J_hist)] = [theta, mse]
-    global iterations 
-    iterations = iterations + 1
-    return mse
-
-
 
 house_prices = pd.read_csv('house_prices.csv', header=None, 
                            names=['area', 'noOfBedrooms', 'price'])
 
+
+# features
+X = house_prices['area'].values
+X = X.reshape(X.shape[0], 1)
+
+# observed predictions
+y = house_prices['price'].values
+y = y.reshape(y.shape[0], 1)
+
+
+# Hypothesis function - h(x) = X.theta
+def hypothesisFunc(theta, X):
+    return X.dot(theta)
+
+
+# Cost Function - Mean Square Error
+def costFunc(theta, X, y):
+    m = X.shape[0]
+    y_pred = hypothesisFunc(theta,X)
+    y_pred = y_pred.reshape(y_pred.shape[0],1)
+    
+    # Calculate mean squared error
+    cost = np.sum((y_pred - y) ** 2)/ (2 * m)
+    J_hist.loc[len(J_hist)] = [theta, cost]
+    
+    
+    global iterations 
+    iterations = iterations + 1
+    return cost
+
+
+
 # initial theta
 initial_theta = np.ones(2).reshape(2,1)
 
-# features
-X = house_prices['area']
-
-# observed predictions
-y = house_prices['price']
 
 # add X0 feature(all ones)
 X = np.column_stack((np.ones(X.shape[0]), X))
@@ -54,7 +69,7 @@ xs = np.linspace(house_prices['area'].min(), house_prices['area'].max(), 10)
 xs = np.column_stack((np.ones(xs.shape[0]), xs))
 
 # ys = prediction for test values of xs
-ys = xs.dot(theta) 
+ys = hypothesisFunc(theta, xs)
 
 
 fig1, axes = pp.subplots()
@@ -115,3 +130,114 @@ ax2.set_ylabel('theta 1')
 fig.colorbar(cs, ax=ax2)
 
 
+########### Using Scaling ############
+
+class MinMaxScaler:
+    def __init__(self):
+        self.mean = 0
+        self.variance = 0
+        
+    def fit(self, f):
+        self.mean = np.mean(f)
+        self.variance = np.max(f) - np.min(f)
+    
+    # f is an numpy array or a series
+    def scale(self, f):
+        return (f - self.mean)/self.variance
+    
+    def inverseScale(self, f):
+        return f * self.variance + self.mean
+    
+
+# clear J_hist
+J_hist = J_hist.iloc[0:0]
+
+
+# features
+X = house_prices['area'].values
+X = X.reshape(X.shape[0], 1)
+
+# observed predictions
+y = house_prices['price'].values
+y = y.reshape(y.shape[0], 1)
+
+
+scaler_X = MinMaxScaler()
+scaler_X.fit(X)    
+X = scaler_X.scale(X)
+
+scaler_Y = MinMaxScaler()
+scaler_Y.fit(y)
+y = scaler_Y.scale(y)
+y = y.reshape(y.shape[0], 1)
+
+# add X0 feature(all ones)
+X = np.column_stack((np.ones(X.shape[0]), X))
+
+
+# train our hypothesis to minimize cost
+res = minimize(costFunc, initial_theta, args=(X, y))
+
+theta = res.x
+
+# Create some test data for which to predict
+xs = np.linspace(house_prices['area'].min(), house_prices['area'].max(), 10)
+xs = scaler_X.scale(xs)
+xs = np.column_stack((np.ones(xs.shape[0]), xs))
+
+ys = hypothesisFunc(theta, xs)
+
+
+# inverse scale everything
+X = scaler_X.inverseScale(X[:,1])
+xs = scaler_X.inverseScale(xs[:, 1])
+
+y = scaler_Y.inverseScale(y)
+ys = scaler_Y.inverseScale(ys)
+
+fig1, axes = pp.subplots()
+axes
+axes.grid(color='b', alpha=0.8, linestyle='dotted', linewidth = 0.5)
+axes.scatter(X, y, marker='o', color='r')
+
+axes.plot(xs,ys, color='blue')
+axes.set_xlabel('area')
+axes.set_ylabel('price')
+
+
+# Plot Progress
+'''
+fig = pp.figure()
+ax = fig.gca()
+theta0_steps = J_hist['theta'].apply(lambda x : x[0])
+theta1_steps = J_hist['theta'].apply(lambda x : x[1])
+J_steps = J_hist['cost']
+
+xl = theta0_steps.min()
+xu = theta0_steps.max()
+yl = theta1_steps.min()
+yu = theta1_steps.max()
+
+# draw contours
+X_scale = scaler_X.scale(X)
+X_scale = np.column_stack((np.ones(X_scale.shape[0]), X_scale))
+y_scale = scaler_Y.scale(y)
+y_scale = y_scale.reshape(y_scale.shape[0], 1)
+
+theta0_vals = np.linspace(2 *xl, 2 * xu, 100)
+theta1_vals = np.linspace(2 *yl, 2 *yu, 100)
+J_vals = np.zeros((len(theta0_vals), len(theta1_vals)))
+T0, T1 = np.meshgrid(theta0_vals, theta1_vals)
+
+for i in range(0, T0.shape[0]):
+    for j in range(0, T0.shape[1]):
+         t = np.array([T0[i,j], T1[i,j]]).reshape(2,)
+         J_vals[i,j] = costFunc(t, X_scale, y_scale)
+
+cs = ax.contour(T0, T1, J_vals, 15,  cmap= cm.hsv, origin = 'lower')
+ax.scatter(theta0_steps, theta1_steps, marker='x', color='r', s=100)
+ax.clabel(cs, inline=True, fontsize=10)
+ax.set_xlabel('theta 0')
+ax.set_ylabel('theta 1')
+fig.colorbar(cs, ax=ax)
+'''
