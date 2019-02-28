27bb4a9c708a74f32e48e9331c16b3612a005c5d
ashish-rane <ashishrane@outlook.com>
2019-02-27 20:46:58 -0400

    2019-02-27

--- Regression/LinearRegression/LinearRegression_ScikitLearn.py
+++ Regression/LinearRegression/LinearRegression_ScikitLearn.py
@@ -33,3 +33,35 @@ y_pred = linreg.predict(X_test.values.reshape((X_test.shape[0], 1)))
 fig, axes = pp.subplots()
 axes.scatter(X, y, color='r')
 axes.plot(X_test, y_pred, color='b', marker='x') 
+
+###################################################################3
+
+house_prices = pd.read_csv('house_prices.csv', header=None, 
+                           names=['area', 'noOfBedrooms', 'price'])
+
+X = house_prices.iloc[:,:-1 ]
+y = house_prices['price']
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# Split for model evaluation
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.3, random_state=101)
+
+# Train our model
+linreg = LinearRegression()
+linreg.fit(X_train, y_train)
+
+# Get Predictions
+y_pred = linreg.predict(X_test)
+
+# Evaluation 
+from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score
+print ('Mean Absolute Error', mean_absolute_error(y_test, y_pred))
+print ('Mean Squared Error', mean_squared_error(y_test, y_pred))
+print ('Root Mean Square Error', np.sqrt(mean_absolute_error(y_test, y_pred)))
+
+print ('R-squared', r2_score(y_test,y_pred))
--- Regression/PolynomialRegression/PolynomialRegression_singleFeature.py
+++ Regression/PolynomialRegression/PolynomialRegression_singleFeature.py
@@ -10,39 +10,46 @@ import pandas as pd
 import matplotlib.pyplot as pp
 from scipy.optimize import minimize
 
+# Hypothesis function
+def hypothesisFunc(theta, X):
+    return X.dot(theta)
+    
+# Cost Function
+def costfunc(theta, X, y):
+    m = X.shape[0]
+    y_pred = hypothesisFunc(theta, X)
+    cost = np.sum((y_pred-y) ** 2)/(2 * m)
+    return cost
+
+# Map higher order polynomial features
 def mapFeatures(X, degree):
     X_poly = X
     for i in range(2, degree + 1):
         X_poly = np.column_stack((X_poly, X ** i))
     return X_poly
 
-def costfunc(theta, X, y):
-    y_pred = X.dot(theta)
-    mse = ((y_pred-y) ** 2).mean()
-    return mse
-
 salaries = pd.read_csv('Position_Salaries.csv')
 
 fig, axes = pp.subplots()
 axes.scatter(salaries['Level'], salaries['Salary'], marker='x', color='r')
-axes.set_xticklabels(salaries['Position'])
+axes.set_xticklabels(salaries['Level'].unique())
 
 degree = 4
 X = salaries['Level'].values
 X = np.column_stack((np.ones(X.shape[0]), mapFeatures(X, degree)))
 y = salaries['Salary'].values
 
-
 initial_theta = np.ones(degree + 1).reshape(degree + 1,1)
 
 res = minimize(costfunc, initial_theta, args=(X,y))
 
 theta = res.x
+theta = theta.reshape(theta.shape[0], 1)
 
 X_test = np.linspace(salaries['Level'].min(), salaries['Level'].max(), 10)
 X_test = np.column_stack((np.ones(X_test.shape[0]), mapFeatures(X_test, degree)))
 
-y_pred = X_test.dot(theta.reshape(theta.shape[0],1))
+y_pred = hypothesisFunc(theta, X_test)
 
 axes.plot(X_test[:,1], y_pred, color='blue')
 
